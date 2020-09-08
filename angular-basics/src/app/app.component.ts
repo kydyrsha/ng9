6c0e1f13b713ebf2065a61cac6c9ef8b71b1b96c
kydyrsha <sanatkydyrsha@gmail.com>
2020-09-08 10:28:20 +0600

    ngFor

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -7,7 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  toggle:any = false
+  arr = [1, 1, 2, 3, 5, 8, 13]
 
-
+  objs = [
+    {title: 'Post 1', author: 'Sanat', comments: [
+      {name:'Kan', text: 'lorem 1'},
+      {name:'Kan', text: 'lorem 1'},
+      {name:'Kan', text: 'lorem 1'}
+    ]},
+    {title: 'Post 2', author: 'SanatV2', comments: [
+      {name:'KanV2', text: 'lorem 2'},
+      {name:'KanV2', text: 'lorem 2'},
+      {name:'KanV2', text: 'lorem 2'}
+    ]}
+  ]
 }
