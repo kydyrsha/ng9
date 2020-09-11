7e8c8c8497f3b37dbbefaffbafb53fbd60f7347d
kydyrsha <sanatkydyrsha@gmail.com>
2020-09-11 12:05:38 +0600

    ts basic types

---
+++ TypeScript_Minin/index.js
@@ -0,0 +1,5 @@
+var str = 'Hello TypeScript';
+var num = 42;
+var isActive = false;
+var strArray = ['H', 'e', 'l'];
+var numArray = [1, 1, 2, 3];
---
+++ TypeScript_Minin/index.ts
@@ -0,0 +1,6 @@
+let str: string = 'Hello TypeScript'
+let num: number = 42
+let isActive: boolean = false
+
+let strArray: string[] = ['H', 'e', 'l']
+let numArray: Array<number> = [1, 1, 2, 3]
