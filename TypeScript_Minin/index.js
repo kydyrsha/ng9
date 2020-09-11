3e9e1f34c68a676737617dcd927dd57839440467
kydyrsha <sanatkydyrsha@gmail.com>
2020-09-11 12:12:08 +0600

    ts functions

--- TypeScript_Minin/index.js
+++ TypeScript_Minin/index.js
@@ -1,5 +1,14 @@
-var str = 'Hello TypeScript';
-var num = 42;
-var isActive = false;
-var strArray = ['H', 'e', 'l'];
-var numArray = [1, 1, 2, 3];
+// let str: string = 'Hello TypeScript'
+// let num: number = 42
+// let isActive: boolean = false
+//
+// let strArray: string[] = ['H', 'e', 'l']
+// let numArray: Array<number> = [1, 1, 2, 3]
+function logInfo(name, age) {
+    console.log("Info: " + name + ", " + age);
+}
+logInfo('Sanat', 23);
+function calc(a, b) {
+    return a + b;
+}
+console.log(calc(2, 3));
