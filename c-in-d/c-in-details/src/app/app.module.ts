3bde2945ce98e751b1e29e692f62a2918023600e
kydyrsha <sanatkydyrsha@gmail.com>
2020-09-14 14:50:02 +0600

    creating component

--- c-in-d/c-in-details/src/app/app.module.ts
+++ c-in-d/c-in-details/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFormComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
